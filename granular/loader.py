35bd81d8151f099ea642a35903d5f15fc0b1f889
Danijar Hafner <mail@danijar.com>
2024-07-04 02:01:25 +0000

    Manual process pool

--- granular/loader.py
+++ granular/loader.py
@@ -1,6 +1,8 @@
 import collections
-import concurrent.futures
 import functools
+import multiprocessing as mp
+import queue
+import time
 from multiprocessing import shared_memory
 
 import numpy as np
@@ -11,7 +13,7 @@ class Loader:
 
   def __init__(
       self, source, fns, batch, shuffle=True, prefetch=10,
-      processes=64, shard_id=0, num_shards=1, seed=0):
+      workers=128, shard_id=0, num_shards=1, seed=0):
 
     self.source = source
     self.fns = fns
@@ -25,14 +27,20 @@ class Loader:
     self.length = len(source)
     self.step = 0
     self.consumed = 0
-    self.started = False
     self.batches = collections.deque()
     self.futures = collections.deque()
 
+    self.started = False
+    self.stop = mp.Event()
+    self.iqueue = mp.Queue()
+    self.oqueue = mp.Queue()
+    self.received = set()
+
+    self.workers = []
     fns = cloudpickle.dumps(fns)
-    initargs = (source, fns, seed)
-    self.pool = concurrent.futures.ProcessPoolExecutor(
-        processes, None, self._init, initargs)
+    args = (self.stop, self.iqueue, self.oqueue, source, fns, seed)
+    for _ in range(workers):
+      self.workers.append(mp.Process(target=self._worker, args=args))
 
   @functools.cached_property
   def spec(self):
@@ -43,17 +51,11 @@ class Loader:
       assert isinstance(datapoint, dict), fn
     return {k: (v.dtype, v.shape) for k, v in datapoint.items()}
 
-  def save(self):
-    return {'step': self.consumed, 'seed': self.seed}
-
-  def load(self, d):
-    self.consumed = self.step = d['step']
-    self.seed = d['seed']
-
   def __iter__(self):
     self.started = True
     for _ in range(self.prefetch):
       self._request()
+    [x.start() for x in self.workers]
     return self
 
   def __next__(self):
@@ -62,28 +64,49 @@ class Loader:
       self._request()
       batch = self._receive()
       self.consumed += self.batch * self.num_shards
-    except (Exception, KeyboardInterrupt, SystemExit):
-      self.pool.shutdown(wait=False, cancel_futures=True)
+    except (SystemExit, KeyboardInterrupt):
+      self.stop.set()
+      time.sleep(0.1)
+      [x.join(timeout=0.01) for x in self.workers]
+      [x.terminate() for x in self.workers]
       raise
     return batch
 
-  @classmethod
-  def _init(cls, source, fns, seed):
-    fns = cloudpickle.loads(fns)
-    globals()['initargs'] = (source, fns, seed)
+  def save(self):
+    return {'step': self.consumed, 'seed': self.seed}
+
+  def load(self, d):
+    self.consumed = self.step = d['step']
+    self.seed = d['seed']
+
+  def close(self):
+    self.stop.set()
 
   @classmethod
-  def _load(cls, index, step, batchdesc, loc):
-    source, fns, seed = globals()['initargs']
-    batch = {k: SharedArray(*v) for k, v in batchdesc.items()}
-    datapoint = source[index]
-    datapoint = {k: np.asarray(v) for k, v in datapoint.items()}
-    for fn in fns:
-      datapoint = fn(datapoint, seed=[seed, step])
-      assert isinstance(datapoint, dict), fn
-    assert datapoint.keys() == batch.keys()
-    for key, value in datapoint.items():
-      batch[key][loc] = value
+  def _worker(cls, stop, iqueue, oqueue, source, fns, seed):
+    fns = cloudpickle.loads(fns)
+    try:
+      while not stop.is_set():
+        try:
+          job = iqueue.get(block=True, timeout=0.1)
+        except queue.Empty:
+          continue
+        index, step, batchdesc, loc = job
+        batch = {k: SharedArray(*v) for k, v in batchdesc.items()}
+        datapoint = source[index]
+        datapoint = {k: np.asarray(v) for k, v in datapoint.items()}
+        for fn in fns:
+          datapoint = fn(datapoint, seed=[seed, step])
+          assert isinstance(datapoint, dict), fn
+        assert datapoint.keys() == batch.keys()
+        for key, value in datapoint.items():
+          batch[key][loc] = value
+        oqueue.put(step)
+    except (SystemExit, KeyboardInterrupt):
+      return
+    except Exception:
+      stop.set()
+      raise
 
   def _request(self):
     batch = {
@@ -94,13 +117,21 @@ class Loader:
     for loc in range(self.batch):
       epoch = self.step // self.length
       index = self._order(epoch)[self.step % self.length]
-      args = (index, self.step, batchdesc, loc)
-      self.futures.append(self.pool.submit(self._load, *args))
+      self.iqueue.put((index, self.step, batchdesc, loc))
       self.step += self.num_shards
 
   def _receive(self):
-    for _ in range(self.batch):
-      self.futures.popleft().result()
+    collected = 0
+    while collected < self.batch:
+      try:
+        step = self.oqueue.get(block=True, timeout=0.1)
+      except queue.Empty:
+        continue
+      self.received.add(step)
+      needed = self.consumed + collected
+      if needed in self.received:
+        self.received.remove(needed)
+        collected += 1
     return self.batches.popleft()
 
   @functools.lru_cache(maxsize=1)
